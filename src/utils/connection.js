9bd49d09d836d5b1047392f8c5d8f0c4905c3d39
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-05-05 20:30:41 +0200

    Try to fix login

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,4 +1,4 @@
 module.exports = function(req) {
-    let proto = req.connection.encrypted || process.env.SSL === 'true' ? 'https' : 'http';
+    let proto = req.connection.encrypted || process.env.SSL === 'true';
     return proto.split(/\s*,\s*/)[0];
 };
