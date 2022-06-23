5da18f0a439576a1f995d5ef640da43c78c3553f
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-06-23 20:09:14 +0200

    Update endpoint

--- src/routes/uonetplus-uczen.js
+++ src/routes/uonetplus-uczen.js
@@ -169,7 +169,7 @@ router.all("/UczenDziennik.mvc/Get", (req, res) => {
                     item.OkresNumer === 2 ? item.IdOkresKlasyfikacyjny : item.IdOkresKlasyfikacyjny + 1
                 ].map((i => {
                     return {
-                        NumerOkresu: i + 1,
+                        NumerOkresu: i,NumerOkresu,
                         Poziom: i.OkresPoziom,
                         DataOd: i.DataOd,
                         DataDo: i.DataDo,
