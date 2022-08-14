aab200af386159682e94e1b71ee6f20e1f463287
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-08-14 17:28:18 +0200

    Fix errors

--- src/routes/uonetplus-wiadomosciplus.js
+++ src/routes/uonetplus-wiadomosciplus.js
@@ -35,3 +35,25 @@ router.get("/api", (req, res) => {
         }
     });
 });
+
+router.get("/OdebraneNowe", (req, res) => {});
+router.get("/WyslaneNowe", (req, res) => {});
+router.get("/LiczbyNieodczytanych", (req, res) => {});
+router.get("/OdebraneSkrzynka", (req, res) => {});
+router.get("/WyslaneSkrzynka", (req, res) => {});
+router.get("/UsunieteSkrzynka", (req, res) => {});
+router.get("/KopieSkrzynka", (req, res) => {});
+router.get("/DdsArchive", (req, res) => {});
+router.get("/Odebrane", (req, res) => {});
+router.get("/Wyslane", (req, res) => {});
+router.get("/Usuniete", (req, res) => {});
+router.get("/Kopie", (req, res) => {});
+router.get("/OdebraneArchiwum", (req, res) => {});
+router.get("/WyslaneArchiwum", (req, res) => {});
+router.get("/UsunieteArchiwum", (req, res) => {});
+router.get("/Ustawienia", (req, res) => {});
+router.get("/Stopka", (req, res) => {});
+router.get("/StatystykiLogowan", (req, res) => {});
+router.get("/Skrzynki", (req, res) => {});
+router.get("/Kopia", (req, res) => {});
+router.get("/OdebraneSzczegolyArchiwum", (req, res) => {});
