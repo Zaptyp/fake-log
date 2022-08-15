2d65bbf23ad79ed66e4ab52c338201a521908cb7
Zaptyp <43276401+Zaptyp@users.noreply.github.com>
2022-08-15 22:26:49 +0200

    Try to fix crash

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressValidator())
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
